fad714217c2b26ba0072fcc2ec47880a10942073
ConnorFleischman <connor.fleischman1@marist.edu>
2024-09-25 10:50:19 -0400

    Class 9/25
    Will cont. notes

--- Practice/solveEverywhere.hs
+++ Practice/solveEverywhere.hs
@@ -1,13 +1,13 @@
 import Data.List (nub)
 
 main :: IO ()
-main = interact (showResult . doTheWork . parseInput)
+main = interact $ showResult . doTheWork . parseInput
 
 parseInput :: String -> [String]
-parseInput = lines . tail
+parseInput = tail . lines
 
 doTheWork :: [String] -> [Int]
-doTheWork = map (length . nub) . splitTrips . tail
+doTheWork = map (length . nub) . splitTrips
   where
     splitTrips [] = []
     splitTrips (x : xs) = take (read x) xs : splitTrips (drop (read x) xs)
